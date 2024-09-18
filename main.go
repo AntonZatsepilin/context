547e0f53b3d9373ade2e4b1ed1bf9d607aa4fa48
AntonZatsepilin <zatsepilin90.00@bk.ru>
2024-09-18 11:27:50 +0300

    1

--- main.go
+++ main.go
@@ -8,12 +8,22 @@ import (
 
 func main() {
 	ctx := context.Background()
-	ctx, _ = context.WithTimeout(ctx, time.Second)
+	ctx, _ = context.WithTimeout(ctx, time.Second*3)
+	ctx = context.WithValue(ctx, "id", 1)
+
+	// ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+
+	// go func() {
+	// 	time.Sleep(time.Millisecond * 100)
+	// 	cancel()
+	// }()
 
 	parse(ctx)
 }
 
 func parse(ctx context.Context) {
+	id := ctx.Value("id")
+	fmt.Println("parsing with id", id.(int))
 	for {
 		select {
 		case <-time.After(time.Second * 2):
